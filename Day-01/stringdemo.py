e653c0808b4a476cd79515240cf584e32b971174
Buchepalli Ramana <buchepalli.ramana@hotmail.com>
2025-02-17 10:08:29 +0530

    string slicing and methods

---
+++ Day-01/stringdemo.py
@@ -0,0 +1,43 @@
+name = "Python programming"
+print(name)
+print("I Love", name)
+
+'''
+#slicing
+# string[start:stop:increment]
+print(len(name)) # number of chars in string
+print(name[0]) # first string
+print(name[-1]) # laast string
+print(name[:-1]) # exclude last string
+print(name[::-1]) # String in reverse order
+print(name[:-3]) #Exclude last three strings
+print(name[0:5]) #Print first 5 items('left' value colon includes and 'right' is excluded)
+print(name[2:5]) #print from third item till 4th index.
+print(name[::]) # all string
+print(name[:]) #All strings
+print(name[0:10:2]) #print every second char starting from 0 to 10 index
+'''
+
+#String Methods
+print(name.capitalize())#first letter will be Uppercase
+print(name.title())#Every first letter of word in sentence will be upper case
+print(name.replace("Python","java"))#replace substring with other string
+print(name.count("p"))#Give the number of occurances of particular string or word
+print(name.upper())#Change all strings into Uppercase
+print(name.lower())#Change all strings into lowercase
+print(name.count("o"))#give the no. of occurances for 'o' in given string
+print(name.startswith("P"))#print the word which starts with given letter
+print(name.endswith("g"))#print the word which ends with given letter
+print(name.isalpha())#Check does it have alpha numeric
+print(name.center(40))#print 40 spaces before and after the string
+print(name.center(40,"*"))#print 40 * before and after the string
+print(name.split(" "))#split the string into multiple items wherever finds the given value
+
+aname = "  Python "
+
+print(len(aname))
+print(len(aname.lstrip()))#remove the left side whitespaces
+print(len(aname.rstrip()))#remove right side whitespaces
+
+
+
